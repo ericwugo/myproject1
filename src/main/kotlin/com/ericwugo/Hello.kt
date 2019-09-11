ebbe5bee04c675d4c19aa450ed3e3fdbb676facf
ericwugo <ericwugo@gmail.com>
2019-09-11 17:49:52 +0800

    gogog

---
+++ src/main/kotlin/com/ericwugo/Hello.kt
@@ -0,0 +1,14 @@
+package com.ericwugo
+
+fun main() {
+//    println("Hello Eric Kotlin world")
+    Human().hello()
+    val h1 = Human()
+    h1.hello()
+
+}
+class Human {
+    fun hello () {
+        println("Hello Kotlinkkk  !  ")
+    }
+}
